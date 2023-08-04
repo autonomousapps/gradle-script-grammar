5c607f61a2a1e3414fba3afc2e819ec6e33f93e2
Tony Robalik <trobalik@squareup.com>
2023-08-04 11:24:27 -0700

    First commit.

---
+++ build-logic/src/main/kotlin/com/autonomousapps/convention/LibConvention.kt
@@ -0,0 +1,25 @@
+package com.autonomousapps.convention
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+
+/**
+ * This plugin is applied to our JVM lib projects.
+ * ```
+ * plugins {
+ *   id("com.autonomousapps.lib")
+ * }
+ * ```
+ */
+class LibConvention : Plugin<Project> {
+
+  override fun apply(target: Project): Unit = target.run {
+    pluginManager.apply(BaseConvention::class.java)
+
+    extensions.configure(JavaPluginExtension::class.java) { j ->
+      j.withJavadocJar()
+      j.withSourcesJar()
+    }
+  }
+}
