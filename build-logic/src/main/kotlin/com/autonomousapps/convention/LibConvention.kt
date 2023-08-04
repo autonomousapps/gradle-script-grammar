24a75045454261c5afbe4893fcd79d17d70a8e0e
Tony Robalik <trobalik@squareup.com>
2023-08-04 11:55:57 -0700

    Finish porting code.

--- build-logic/src/main/kotlin/com/autonomousapps/convention/LibConvention.kt
+++ build-logic/src/main/kotlin/com/autonomousapps/convention/LibConvention.kt
@@ -1,5 +1,6 @@
 package com.autonomousapps.convention
 
+import com.autonomousapps.convention.publishing.Publishing
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
@@ -16,10 +17,13 @@ class LibConvention : Plugin<Project> {
 
   override fun apply(target: Project): Unit = target.run {
     pluginManager.apply(BaseConvention::class.java)
+    pluginManager.apply("com.github.johnrengelman.shadow")
 
     extensions.configure(JavaPluginExtension::class.java) { j ->
       j.withJavadocJar()
       j.withSourcesJar()
     }
+
+    Publishing.setup(this)
   }
 }
